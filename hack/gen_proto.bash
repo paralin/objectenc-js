2cb96545ec17959e3018ecd039cd736593eb2456
Christian Stewart <christian@paral.in>
2018-01-12 18:24:01 -0500

    chore: gen protobufs from submodule

--- hack/gen_proto.bash
+++ hack/gen_proto.bash
@@ -3,19 +3,21 @@ set -eo pipefail
 
 cd $(git rev-parse --show-toplevel)
 if [ ! -d ../objectenc ]; then
-    echo "Clone objectenc to ../"
-    echo "git clone git@github.com:aperturerobotics/objectenc.git $(pwd)/../objectenc"
+    echo "Please run this from the JS monorepo"
     exit 1
 fi
 
 finalize() {
     pbts -o ./src/pb/${1}.d.ts ./src/pb/${1}.js
     sed -i '1s;^;/* tslint:disable */\n;' ./src/pb/${1}.d.ts
+    git add ./src/pb/${1}.d.ts ./src/pb/${1}.js
 }
 
 export PATH=$PATH:$(pwd)/node_modules/.bin
-pbjs -t static-module -w commonjs -o ./src/pb/objectenc.js ../objectenc/objectenc.proto
+pbjs -t static-module -w commonjs -o ./src/pb/objectenc.js ./proto/objectenc.proto
 finalize objectenc
 
-pbjs -t static-module -w commonjs -o ./src/pb/aes.js ../objectenc/aes/aes.proto
+pbjs -t static-module -w commonjs -o ./src/pb/aes.js ./proto/aes/aes.proto
 finalize aes
+
+npm run precommit
