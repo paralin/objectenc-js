03f7d8f0831aa022cb8606087575a6b273cd1d65
Christian Stewart <christian@paral.in>
2018-02-06 02:51:46 -0500

    fix: upgrade openssl and fix crypto

--- crypto.ts
+++ crypto.ts
@@ -1,7 +1,9 @@
 let webCrypto = window.crypto
-import * as NodeWebCrypto from 'node-webcrypto-ossl'
+// import * as NodeWebCrypto from 'node-webcrypto-ossl'
 
+declare var require: any
 if (!webCrypto) {
-  webCrypto = new NodeWebCrypto()
+  let wc: any = require('node-webcrypto-ossl')
+  webCrypto = new wc()
 }
 export const crypto = webCrypto
